aee29c4a5f9cb569058a528b43dbd7e9079cb127
kathleenxie <61072852+kathleenxie@users.noreply.github.com>
2024-02-23 15:18:07 -0800

    Update main.py

--- PredatorAndPrey/main.py
+++ PredatorAndPrey/main.py
@@ -1,25 +1,12 @@
-# filename: OO - Super Turtles - Project 1 - WIP
 from random import randint
 from superturtle import *
 import time
-
-# make sure to implement a class that supports the following:
 
 prey = Superturtle(100, -40, 90, 5, 'blue')       # (x, y, heading, step, color)
 prey.start_trace()
 
 predator = Superturtle(60, 90, 90, 10, 'red')    # (x, y, heading, step, color)
 predator.start_trace()                          # lower the pen to leave a trace as the superturtle moves
-
-# The Pseudo Code:
-'''
-while the distance between the predator and the prey is bigger than the predator step size:
-  the predator moves forward - based on its step size
-  if the smell of the prey is "weaker" after this step, compared to the smell before:
-    the predator turns left by a random amount in the range of 1-90 degrees
-'''
-
-# Your code here:
 
 prev_smell = 0    
 curr_smell = predator.distance_to(prey)
